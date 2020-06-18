ce866d66a18e711c11a9473ac2746eb0e987ac4d
Rushi <rushikeshp110@gmail.com>
2020-06-18 10:04:33 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require ("express");
+const bodyParser = require ("body-parser");
+const ejs = require ("ejs");
+
+const app = express();
+app.set("view engine" , "ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use( express.static( "public" ) );
+
+app.get("/",function(req,res){
+  res.render("home");
+})
+
+app.get("/home",function(req,res){
+  res.render("home");
+})
+
+app.get("/contactUs",function(req,res){
+  res.render("contactUs");
+})
+
+app.get("/theTeam",function(req,res){
+  res.render("theTeam");
+})
+
+app.listen(3000,function(){
+  console.log("server is running on port 3000");
+});
